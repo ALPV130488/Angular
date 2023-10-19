2ffeee5e96337c3209fd259c30b2b318b744ed8d
LAKSHMI A <108331858+ALPV488@users.noreply.github.com>
2023-10-19 17:04:04 +0530

    Add files via upload

---
+++ visual/add.js
@@ -0,0 +1,18 @@
+// function add( num1 , num2){
+
+//     return num1 + num2 ;
+// }
+
+
+//              ctrl+/
+
+let add = function(num1,num2){
+    console.log("hi pooja");
+return num1+num2;
+
+}
+
+let sum=add;
+let result = sum(2,3);//called the function
+
+console.log(result);
---
+++ visual/app.js
@@ -0,0 +1,12 @@
+function greet(u){
+
+    return `Hello ${u}`
+}
+
+let user ="Pooja";
+
+let user1 = "Anitha";
+
+let str = greet(user1);
+
+console.log(str);
